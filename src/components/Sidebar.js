f3aaa96196ef8d38998f0e2f58d34ffb72364a81
yashwith26 <srysmk483@gmail.com>
2024-12-15 12:15:16 +0530

    Feature: Live Chat

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,10 +11,20 @@ const Sidebar = () => {
     <div className="p-5 shadow-lg w-48">
       <ul>
         <li className="text-lg">
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <p className="font-semibold">
+              <i className="fa-solid fa-house pr-2"></i>Home
+            </p>
+          </Link>
         </li>
-        <li className="text-lg">Shorts</li>
-        <li className="text-lg">Subscriptions</li>
+        <li className="text-lg">
+          <p className="font-semibold">
+            <i className="fa-solid fa-video pr-2"></i>Shorts
+          </p>
+        </li>
+        <p className="font-semibold">
+          <li className="text-lg">Subscriptions</li>
+        </p>
       </ul>
 
       <h1 className="font-bold text-lg pt-5">You</h1>
@@ -23,7 +33,7 @@ const Sidebar = () => {
         <li>Playlists</li>
         <li>Your Videos</li>
         <li>Watch Later</li>
-        <li>Lieked Videos</li>
+        <li>Liked Videos</li>
       </ul>
       <h1 className="font-bold text-lg pt-5">Subscriptions</h1>
       <ul>
---
+++ src/utils/searchSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState: {},
+
+  reducers: {
+    cacheResults: (state, action) => {
+      Object.assign(state, action.payload);
+    },
+  },
+});
+
+export default searchSlice.reducer;
+export const { cacheResults } = searchSlice.actions;
